e1729ee0a51d51d1484b91a1292a67f76499e51f
43MorenoLuis <lmoreno9943@gmail.com>
2021-12-21 19:25:02 -0500

    Integracion y configuraciones iniciales con redux

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+
+import { store } from './store/store';
 import AppRouter from './routers/AppRouter';
 
 export default function App() {
   return (
-    <AppRouter />
+    <Provider store={ store }>
+      <AppRouter />
+    </Provider>
   )
 }
 
