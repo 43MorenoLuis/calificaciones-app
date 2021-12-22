a630a904eae664a47861dfcc3be43346e19843ad
43MorenoLuis <lmoreno9943@gmail.com>
2021-12-22 13:43:31 -0500

    Se agrego react-big-calendar y moment para los eventos

---
+++ client/src/components/calendar/CalendarScreen.js
@@ -0,0 +1,78 @@
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+
+import moment from 'moment';
+import 'moment/locale/es';
+
+import NavbarScreen from '../ui/navbar/NavbarScreen';
+import { messages } from '../../helpers/calendar-messages-es';
+import CalendarEvent from './CalendarEvent';
+
+const localizer = momentLocalizer(moment) // or globalizeLocalizer
+
+const events = [{
+    title: 'Cumpleaños de Yu',
+    start: moment().toDate(),
+    end: moment().add(2, 'hours').toDate(),
+    bgcolor: '#fafafa',
+    notes: 'Comprar pastel',
+    user: {
+        _id: '123',
+        name: '43MorenoLuis'
+    }
+}];
+
+moment.locale('es');
+
+export default function CalendarScreen() {
+
+    const [lastView, setLastView] = useState( localStorage.getItem('lastView') || 'month' );
+
+    const eventStyleGetter = ( event, start, end, isSelected ) => {
+        const style = {
+            backgroundColor: '#367CF7',
+            borderRadius: '0px',
+            opacity: 0.8,
+            display: 'block',
+            color: 'white'
+        }
+
+        return {
+            style
+        }
+    }
+
+    const onDoubleClick = (e) => {
+        console.log(e);
+    }
+
+    const onSelectEvent = (e) => {
+        console.log(e);
+    }
+
+    const onViewChange = (e) => {
+        setLastView(e);
+        localStorage.setItem('lastView', e);
+    }
+
+    return (
+        <div className='__calendar-screen'>
+            <NavbarScreen />
+            <Calendar
+                localizer={localizer}
+                events={events}
+                startAccessor="start"
+                endAccessor="end"
+                messages={ messages }
+                eventPropGetter = { eventStyleGetter }
+                onDoubleClickEvent={ onDoubleClick }
+                onSelectEvent={ onSelectEvent }
+                onView={ onViewChange }
+                view={ lastView }
+                components={{
+                    event: CalendarEvent
+                }}
+            />
+        </div>
+    )
+}
--- client/src/routers/AppRouter.js
+++ client/src/routers/AppRouter.js
@@ -7,6 +7,7 @@ import {
 
 import LoginScreen from '../components/auth/login/LoginScreen';
 import RegisterScreen from '../components/auth/register/RegisterScreen';
+import CalendarScreen from '../components/calendar/CalendarScreen';
 import HomeScreen from '../components/home/HomeScreen';
 import NotFoundScreen from '../components/notFound/NotFoundScreen';
 
@@ -16,8 +17,9 @@ export default function AppRouter() {
             <div>
                 <Routes>
                     <Route path="/" element={ <HomeScreen/> } />
-                    <Route path="login" element={ <LoginScreen/> } />
-                    <Route path="register" element={ <RegisterScreen/> } />
+                    <Route path="/login" element={ <LoginScreen/> } />
+                    <Route path="/register" element={ <RegisterScreen/> } />
+                    <Route path="/calendar" element={ <CalendarScreen/> } />
                     <Route exact path="*" element={ <NotFoundScreen/> } />
                 </Routes>
             </div>
